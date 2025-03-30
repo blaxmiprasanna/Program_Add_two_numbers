d9bc9c7f817703a8c78631b3096cf6ebebcd34bf
blaxmiprasanna <blaxmiprasanna@yahoo.com>
2025-03-29 22:28:23 -0500

    new maven project

---
+++ src/main/java/com/Program_Add_two_numbers/App.java
@@ -0,0 +1,13 @@
+package com.Program_Add_two_numbers;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
