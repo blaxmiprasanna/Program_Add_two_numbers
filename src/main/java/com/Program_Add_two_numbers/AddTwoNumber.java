429eecbbb1c7bdad3758b1bc71c0a569de99a10b
blaxmiprasanna <blaxmiprasanna@yahoo.com>
2025-03-30 18:05:34 -0500

    AddTwoNumber.java file is added in src/main/java path

---
+++ src/main/java/com/Program_Add_two_numbers/AddTwoNumber.java
@@ -0,0 +1,13 @@
+package com.Program_Add_two_numbers;
+
+public class AddTwoNumber {
+
+	public static void main(String[] args) {
+		
+		System.out.println(addTwoNumber(10, 10));
+	}
+	
+	public static int addTwoNumber(int a, int b){
+		return a + b;
+	}
+}
