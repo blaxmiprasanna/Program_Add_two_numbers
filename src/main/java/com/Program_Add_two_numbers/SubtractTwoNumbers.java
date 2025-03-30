0dd551c4dd83b86bc1d3a9087cd9934a0e0e1a77
lakshmikanthambottysundar <lakshmikantham.bottysundar@gmail.com>
2025-03-30 18:35:43 -0500

    Added SubtractTwoNumbers java file

---
+++ src/main/java/com/Program_Add_two_numbers/SubtractTwoNumbers.java
@@ -0,0 +1,13 @@
+package com.Program_Add_two_numbers;
+
+public class SubtractTwoNumbers {
+
+    public static void main (String args[]){
+        System.out.println(subtractTwoNumbers(10,6));
+    }
+
+    public static int subtractTwoNumbers(int a, int b){
+
+        return a-b;
+    }
+}
