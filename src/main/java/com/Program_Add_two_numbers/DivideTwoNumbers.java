fa804a2a34ea85e246e6533978e71d1bcdfda7cb
blaxmiprasanna <blaxmiprasanna@yahoo.com>
2025-04-03 21:25:43 -0500

    added divideTwoNumber java program

---
+++ src/main/java/com/Program_Add_two_numbers/DivideTwoNumbers.java
@@ -0,0 +1,13 @@
+package com.Program_Add_two_numbers;
+
+public class DivideTwoNumbers {
+
+	public static void main(String[] args) {
+		
+		System.out.println(divide(10,5));
+	}
+	
+	public static int divide(int a, int b){
+		return a/b;
+	}
+}
